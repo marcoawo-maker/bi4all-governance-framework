ba4af7e0d07eeb51481f443995340c5850bd1aa7
marcoawo-maker <marco.a.w.o@gmail.com>
2026-04-16 23:19:56 +0100

    Update 05_permissions.sql
    [Docs] Update and align documentation with latest governance baseline implementation

--- sql/governance/Core/05_permissions.sql
+++ sql/governance/Core/05_permissions.sql
@@ -1,34 +1,26 @@
-<<<<<<< HEAD
 /*
-Minimum permissions required for Power Apps / Power Automate SQL connector identity.
-
+Minimum permissions required for the Power Apps / Power Automate SQL connector identity.
 Replace <principal> with your managed identity / service principal / user.
 */
 
 -- Read for UI
 -- GRANT SELECT ON OBJECT::admin.v_ui_copyDataConfig TO [<principal>];
 -- GRANT SELECT ON OBJECT::admin.v_ui_copyDataConfig_fields TO [<principal>];
--- (optional)
+-- GRANT SELECT ON OBJECT::admin.v_ui_copyDataConfig_search TO [<principal>];
 -- GRANT SELECT ON OBJECT::admin.v_ui_copyDataConfig_search_v2 TO [<principal>];
+-- GRANT SELECT ON OBJECT::admin.v_runPreview_orphans TO [<principal>];
+-- GRANT SELECT ON OBJECT::admin.v_runPreview_readiness TO [<principal>];
+-- GRANT SELECT ON OBJECT::admin.v_runPreview_readinessMessage TO [<principal>];
+-- GRANT SELECT ON OBJECT::admin.v_runPreview_lastRun TO [<principal>];
+-- GRANT SELECT ON OBJECT::admin.v_runPreview_processStatus TO [<principal>];
+-- GRANT SELECT ON OBJECT::admin.v_runPreview_readiness_v2 TO [<principal>];
+-- GRANT SELECT ON OBJECT::admin.vw_copyDataConfig_Yaml TO [<principal>];
+-- GRANT SELECT ON OBJECT::admin.vw_copyDataConfig_Yaml_Latest TO [<principal>];
 
 -- Execute for flows
 -- GRANT EXECUTE ON OBJECT::admin.usp_CreateCopyDataConfig_Basic TO [<principal>];
 -- GRANT EXECUTE ON OBJECT::admin.usp_SetFlagActive TO [<principal>];
-=======
-/*
-Minimum permissions required for Power Apps / Power Automate SQL connector identity.
-
-Replace <principal> with your managed identity / service principal / user.
-*/
-
--- Read for UI
--- GRANT SELECT ON OBJECT::admin.v_ui_copyDataConfig TO [<principal>];
--- GRANT SELECT ON OBJECT::admin.v_ui_copyDataConfig_fields TO [<principal>];
--- (optional)
--- GRANT SELECT ON OBJECT::admin.v_ui_copyDataConfig_search_v2 TO [<principal>];
-
--- Execute for flows
--- GRANT EXECUTE ON OBJECT::admin.usp_CreateCopyDataConfig_Basic TO [<principal>];
--- GRANT EXECUTE ON OBJECT::admin.usp_SetFlagActive TO [<principal>];
->>>>>>> 88bbf1aad7e883f8c19e0d3796f34cc884d3698b
 -- GRANT EXECUTE ON OBJECT::admin.usp_SetFlagBlock TO [<principal>];
+-- GRANT EXECUTE ON OBJECT::admin.usp_CreateCopyDataConfig_UI TO [<principal>];
+-- GRANT EXECUTE ON OBJECT::admin.usp_UpdateCopyDataConfig_UI TO [<principal>];
+-- GRANT EXECUTE ON OBJECT::admin.usp_SetCopyDataConfigActive_UI TO [<principal>];
